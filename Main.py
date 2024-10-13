df315de2e4ec7ab6dbfeabe01840c2a05b72dd03
TreasureCoded-locked-in <akpannsikak410@gmail.com>
2024-10-13 16:30:01 +0100

    First Commit

---
+++ Main.py
@@ -0,0 +1,15 @@
+# Print a Sentence
+print('Welcome to the world of programming!')
+
+#Print a number
+print('4')
+
+#Next line after Print
+print('Hello python \n')
+
+#Pass possible arguments for printing
+print('hello',5)
+
+#End argument of Print statement
+print('Welcome to ', end='*')
+
